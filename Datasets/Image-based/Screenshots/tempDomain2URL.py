d11c3b22b17a95066c31a9b643f30ac13628696d
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-03-01 20:35:00 -0600

    Update tempDomain2URL.py

--- Datasets/Image-based/Screenshots/tempDomain2URL.py
+++ Datasets/Image-based/Screenshots/tempDomain2URL.py
@@ -1,5 +1,5 @@
 # This is a quick file that I'll delete later on; it's for renaming the domains to urls in the file
-# I haven't actually ran this yet; I'll run it when I get a chance and just end up debugging the other code (combined.py)
+# I've already ran it on my dataset, but if you want to use it on another dataset where you only know domains it may come in handy to get the full URLs
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
@@ -15,11 +15,7 @@ driver.get('https://www.google.com/')
 url = driver.current_url
 element_present = EC.visibility_of_all_elements_located((By.TAG_NAME, "a"))
 WebDriverWait(driver, 10).until(element_present)
-print("worK")
 
-# for element in aTags:
-#     href = element.get_attribute("href")
-#     print(href)
 
 driver.close()
 driver.quit()
