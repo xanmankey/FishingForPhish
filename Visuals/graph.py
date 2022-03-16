dd24769396b8f7fb554e4e206748c9b38113564f
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-03-16 12:47:02 -0500

    Update graph.py

--- Visuals/graph.py
+++ Visuals/graph.py
@@ -1,4 +1,7 @@
 # TODO: Using matplotlib to generate graphs for research purposes
 # TODO: Or use the WEKA graphs; I haven't figured out which yet
+# REMEMBER: Only include important graphs. This means DON'T include graphs for every single attribute and instance
+# I need to use WEKA visualizations to search for important patterns and data (those are the only figures I might include)
+# I'll NEED TO MENTION that in order to view graphs, std deviation, mean, and other data, the data should be loaded in WEKA GUI
 import matplotlib
 
