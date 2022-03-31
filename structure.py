077ab15bb11f93fd308108ff16d1540506a06c90
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-03-31 09:04:18 -0500

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,16 @@
+# Just trying to gather my ideas; the idea of easily being able to add an "analyzer" so to speak
+# is one of my goals; this code could do it
+class initialize():
+  analyzers = []
+  
+  
+# my first base class
+class scrape(initialize):
+  
+# my second base class
+class data(initialize):
+  
+class analyzer1(scrape, data):
+  
+  
+class analyzer2(scrape, data):
