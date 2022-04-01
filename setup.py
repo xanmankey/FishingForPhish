dd6d2687a34114041a12f2fead1c3458f957a107
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-04-01 11:38:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 import pathlib
 
+repo = pathlib.Path(__file__).parent.resolve()
+long_description = (repo / "README.md").read_text(encoding="utf-8")
+
+setup(
+
+
+
+)
 # Setup.py file; initializes parsing of setup.cfg
 if __name__ == "__main__":
     setup()
