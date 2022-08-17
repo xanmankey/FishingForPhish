5fb76cce2a021618da4dc5cd8a0cdabe24fc525d
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-17 13:59:28 +0000

    ddd, Aug 8/17/2022, YYYY, 1:59 A +0

--- src/FishingForPhish/FishingForPhish.py
+++ src/FishingForPhish/FishingForPhish.py
@@ -20,7 +20,7 @@ from selenium.webdriver.support import expected_conditions
 from sklearn import feature_selection
 # Not sure if you can create datasets using sklearn (I might want to load them instead)
 # I think maybe I'll use pandas instead
-from sklearn import datasets
+import pandas as pd
 # for graphing
 from matplotlib import pyplot as plt
 from matplotlib import style
