8119149366a249b78762ec78a6104f50c196bd4e
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-18 13:45:36 +0000

    ddd, Aug 8/18/2022, YYYY, 1:45 A +0

--- src/FishingForPhish/FishingForPhish.py
+++ src/FishingForPhish/FishingForPhish.py
@@ -100,7 +100,7 @@ class startFishing():
                 "errors": """CREATE TABLE errors (url TEXT UNIQUE, error TEXT)""",
                 "hashes": """CREATE TABLE hashes (phash TEXT, dhash TEXT, url TEXT)"""
             }
-            if (for file in os.listdir(self.dataDir + "datasets")):
+            if any(for file in os.listdir(self.dataDir + "datasets")):
                 try:
                     self.conn = sqlite3.connect(self.database)
                 except Exception:
