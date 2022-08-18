783aaf8e18d3d5052d5fcc2c267fd70425259da1
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-18 13:14:57 +0000

    ddd, Aug 8/18/2022, YYYY, 1:14 A +0

--- src/FishingForPhish/FishingForPhish.py
+++ src/FishingForPhish/FishingForPhish.py
@@ -38,7 +38,9 @@ from requests.exceptions import RequestException
 import os
 import sys
 from filetype import is_image
-import sqlite3
+## Replaced sqlite3 with postgres (the PyPi module is well-abstracted anyway)
+# import sqlite3
+from postgres import Postgres
 import cssutils
 import logging
 from datetime import datetime
