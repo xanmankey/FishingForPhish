a718ea74adc9d967d244a5adafce27681a37b6f5
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-22 12:35:10 +0000

    ddd, Aug 8/22/2022, YYYY, 12:35 A +0

---
+++ tests(TODO)/postgresTest.py
@@ -0,0 +1,15 @@
+# Testing postgres
+from postgres import Postgres
+
+def main():
+    # Create db
+    db = Postgres()
+    # Create table
+    
+    # Insert db
+
+    # Select db
+
+
+if __name__ == "__main__":
+    main()
