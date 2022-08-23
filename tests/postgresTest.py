fd023f4879ec7d0b0a21ed3a18716723b2e77a95
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-23 13:00:50 +0000

    ddd, Aug 8/23/2022, YYYY, 1:00 A +0

--- tests/postgresTest.py
+++ tests/postgresTest.py
@@ -1,5 +1,7 @@
 # Testing postgres
 # 3 files for testing basic functionality ALONE and then specific tests for the code itself
+# Original clang CFLAGS:
+# '-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow'
 from postgres import Postgres
 
 def main():
