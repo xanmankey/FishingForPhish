014cd123e16d4d78e834db330f35fb7a5745ace6
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-23 13:59:38 +0000

    ddd, Aug 8/23/2022, YYYY, 1:59 A +0

--- tests/postgresTest.py
+++ tests/postgresTest.py
@@ -2,7 +2,7 @@
 # 3 files for testing basic functionality ALONE and then specific tests for the code itself
 # Original clang CFLAGS:
 # '-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow'
-# There's no file there; do I need to start a Postgresql server or is it an issue with compiled via env (apt) files?
+# Decided to use psycopg2 for the extra control
 import psycopg2
 
 def main():
