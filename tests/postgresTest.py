13fb1b77be2cc5f7588a7e9879b8c49a2b8f64d1
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-23 14:00:47 +0000

    ddd, Aug 8/23/2022, YYYY, 2:00 A +0

--- tests/postgresTest.py
+++ tests/postgresTest.py
@@ -2,7 +2,7 @@
 # 3 files for testing basic functionality ALONE and then specific tests for the code itself
 # Original clang CFLAGS:
 # '-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow'
-# Decided to use psycopg2 for the extra control
+# Decided to test using psycopg2 for the extra control (I might migrate back to Postgres testing, but I have to figure out this error first)
 import psycopg2
 
 def main():
