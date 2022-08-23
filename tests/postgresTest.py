76a5a557477a9f4e94b4618a8b41de0cfbaaa6a9
xanmankey <86136010+xanmankey@users.noreply.github.com>
2022-08-23 13:58:38 +0000

    ddd, Aug 8/23/2022, YYYY, 1:58 A +0

--- tests/postgresTest.py
+++ tests/postgresTest.py
@@ -2,20 +2,22 @@
 # 3 files for testing basic functionality ALONE and then specific tests for the code itself
 # Original clang CFLAGS:
 # '-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow'
-# Current error: psycopg2.OperationalError: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: No such file or directory
-        # Is the server running locally and accepting connections on that socket?
 # There's no file there; do I need to start a Postgresql server or is it an issue with compiled via env (apt) files?
-from postgres import Postgres
+import psycopg2
 
 def main():
     # Create db
-    db = Postgres()
+    con = psycopg2.connect(database="testDB", user="postgres", password="", host="127.0.0.1", port="5432")
+    cur = con.cursor()
     # Create table
-    db.run("CREATE TABLE test (name text)")
+    cur.execute("CREATE TABLE test (name text)")
     # Insert db
-    db.run("INSERT INTO test (name) VALUES ('jimmy')")
+    cur.execute("INSERT INTO test (name) VALUES ('jimmy')")
+    con.commit()
     # Select db
-    db.run("SELECT * FROM test")
+    cur.execute("SELECT * FROM test")
+    # End db connection
+    con.close()
 
 
 if __name__ == "__main__":
